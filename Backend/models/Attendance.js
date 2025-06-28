705a6fab35ad1636dc4a131aa5135c283cf5ef68
Gaurav-Attri <attrigaurav60@gmail.com>
2025-06-28 09:08:16 +0530

    Entity Models Creation Done

---
+++ Backend/models/Attendance.js
@@ -0,0 +1,48 @@
+const mongoose = require('mongoose');
+
+const attendanceSchema = new mongoose.Schema({
+    employeeId: {
+        type: Number,
+        required: true
+    },
+    clockInTime: {
+        type: Date,
+        required: true
+    },
+    clockOutTime: {
+        type: Date
+    },
+    totalHoursWorked: {
+        type: Number,
+        min: 0
+    },
+    date: {
+        type: Date,
+        required: true
+    },
+    isLate: {
+        type: Boolean,
+        default: false
+    },
+    status: {
+        type: String,
+        enum: ["present", "absent"],
+        default: "present"
+    },
+    location: {
+        latitude: { 
+            type: Number,
+            required: true,
+            min: -90,
+            max: 90
+        },
+        longitude: {
+            type: Number,
+            required: true,
+            min: -180,
+            max: 180
+        }
+    }
+}, {timestamps: true});
+
+module.exports = mongoose.model("Attendance", attendanceSchema);
---
+++ Backend/models/Employee.js
@@ -0,0 +1,43 @@
+const mongoose = require('mongoose');
+
+const employeeSchema = new mongoose.Schema({
+    firstName: {
+        type: String,
+        required: true,
+        trim: true
+    },
+    lastName: {
+        type: String,
+        required: true,
+        trim: true
+    },
+    email: {
+        type: String,
+        required: true,
+        unique: true,
+        index: true,
+        lowercase: true,
+        trim: true
+    },
+    hashedPassword: {
+        type: String,
+        required: true
+    },
+    role: {
+        type: String,
+        enum: ["employee", "admin"],
+        default: "employee"
+    },
+    isActive: {
+        type: Boolean,
+        default: true
+    },
+    employeeId: {
+        type: Number,
+        required: true,
+        unique: true,
+        index: true
+    }
+}, {timestamps: true});
+
+module.exports = mongoose.model("Employee", employeeSchema);
