092232097ee9705c4e198a55a5e192d9688a4fc8
Gaurav-Attri <attrigaurav60@gmail.com>
2025-07-02 16:11:02 +0530

    Created the functionality (controller function and route) to allow admin to see the attendance records of employees

--- Backend/controllers/adminController.js
+++ Backend/controllers/adminController.js
@@ -1,4 +1,5 @@
 const Employee = require('../models/Employee');
+const Attendance = require('../models/Attendance');
 
 const getEmployeeList = async (req, res) => {
     try{    
@@ -10,6 +11,41 @@ const getEmployeeList = async (req, res) => {
     }
 }
 
+const getAttendanceRecords = async (req, res) => {
+    try{
+        const {employeeId, date} = req.query;
+        const query = {};
+
+        // converting numeric employeeId to objectId
+        if(employeeId){
+            const employee = await Employee.findOne({employeeId: Number(employeeId)});
+            if(!employee){
+                return res.status(404).json({message: "Employee Not Found."});
+            }
+            query.employeeId = employee._id;
+        }
+
+        if(date){
+            const dayStart = new Date(date);
+            dayStart.setHours(0, 0, 0, 0);
+
+            const dayEnd = new Date(date);
+            dayEnd.setHours(23, 59, 59, 999);
+
+            query.clockInTime = {$gte: dayStart, $lte: dayEnd};
+        }
+
+        const records = await Attendance.find(query).populate("employeeId", "firstName lastName email employeeId").sort({clockInTime: -1});
+
+        return res.json(records);
+
+    }catch(error){
+        console.error("Error Fetching attendance records.", error);
+        return res.status(500).json({message: "Server Error"});
+    }
+}
+
 module.exports = {
-    getEmployeeList
+    getEmployeeList,
+    getAttendanceRecords
 };
--- Backend/routes/adminRoutes.js
+++ Backend/routes/adminRoutes.js
@@ -5,5 +5,5 @@ const verifyToken = require("../middleware/verifyToken");
 const adminController = require("../controllers/adminController");
 
 router.get("/employees", verifyToken, isAdmin, adminController.getEmployeeList);
-
+router.get("/employees/attendance/records", verifyToken, isAdmin, adminController.getAttendanceRecords);
 module.exports = router;
