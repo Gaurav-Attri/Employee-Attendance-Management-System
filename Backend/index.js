431eb1f9f1d397a3adb3c4f985c103eddf0c4f1d
Gaurav-Attri <attrigaurav60@gmail.com>
2025-07-19 11:36:13 +0530

    Fixed CORS error in the backend

--- Backend/index.js
+++ Backend/index.js
@@ -4,9 +4,11 @@ const connectDB = require('./config/db');
 const authRoutes = require('./routes/authRoutes');
 const attendanceRoutes = require('./routes/attendanceRoutes');
 const adminRoutes = require('./routes/adminRoutes');
+const cors = require('cors');
 
 const app = express();
 
+app.use(cors());
 app.use(express.json());
 
 app.use('/api/auth', authRoutes);
